7ce8b67fee76c14ec32d9beb1a5d9e9f0939bcc8
choksis8168 <shreya.choksi@yahoo.com>
2024-01-22 10:49:23 -0500

    fixing issue #2

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,30 +1,15 @@
 // Endpoint for querying the fibonacci numbers
 
-// import fibonacci from "./fib";
-
-// export default (req, res) => {
-//   const { num } = req.params;
-
-//   const fibN = fibonacci(parseInt(num));
-//   let result = `fibonacci(${num}) is ${fibN}`;
-
-//   if (fibN < 0) {
-//     result = `fibonacci(${num}) is undefined`;
-//   }
-
-//   res.send(result);
-// };
 import fibonacci from "./fib";
+import { Request, Response } from "express"; // Assuming you're using Express
 
-export default (req: any, res: any) => { // Add type annotations for req and res
+export default (req: Request, res: Response) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
-  let result: string; // Specify the type of 'result'
+  let result = `fibonacci(${num}) is ${fibN}`;
 
-  if (fibN !== undefined) { // Check if fibN is not undefined
-    result = `fibonacci(${num}) is ${fibN}`;
-  } else {
+  if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
   }
 
